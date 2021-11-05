a2ddbfff8e25a7d0d58b031ccd521e6b2c836df2
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-11-05 16:42:32 +0100

    module cleanup shared module now re-exports common and material modules for other modules to use; some other cleanup, products routing module and formatting via prettier

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { AuthenticationModule } from '../authentication/authentication.module';
+import { ProductsModule } from '../products/products.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [AuthenticationModule, ProductsModule],
 })
 export class CoreModule {}
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [SharedModule, ProductsRoutingModule],
 })
 export class ProductsModule {}
