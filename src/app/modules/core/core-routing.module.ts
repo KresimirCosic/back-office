ba2eb9eadc18282f5336ab513db3060ca5c2fefa
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-11-05 16:24:38 +0100

    modules added most if not all modules required for the app

---
+++ src/app/modules/core/core-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'authentication',
+    loadChildren: () =>
+      import('../authentication/authentication.module').then(
+        (module) => module.AuthenticationModule
+      ),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('../products/products.module').then(
+        (module) => module.ProductsModule
+      ),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
