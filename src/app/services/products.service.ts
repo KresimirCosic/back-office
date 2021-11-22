4dfb85b255ca201c4d4034cf7bf0946765a9617d
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-11-22 14:05:10 +0100

    products service started work on products service; added state methods to the class mostly

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { cloneDeep } from 'lodash';
 
 import { APIService } from './api.service';
 import { IProductsState } from '../models/state/products-state.model';
@@ -15,8 +16,27 @@ export class ProductsService extends APIService {
     APIRequests: [],
   });
   readonly productsState$ = this._productsState.asObservable();
+  private readonly _productsAPI: string = `${this.API}/products`;
+  private readonly _statsAPI: string = `${this.API}/stats/categories`;
 
   constructor() {
     super();
+  }
+
+  private _cloneState(): IProductsState {
+    return cloneDeep(this._productsState.getValue());
+  }
+
+  private _updateState(productsStateValue: Partial<IProductsState>): void {
+    this._productsState.next({
+      ...this._cloneState(),
+      ...productsStateValue,
+    });
+  }
+
+  private _removeAPIRequestID(newAPIRequestID: string): string[] {
+    return this._cloneState()['APIRequests'].filter(
+      (APIRequestID) => APIRequestID !== newAPIRequestID
+    );
   }
 }
