9bef4e454a0b028a48f91495344767c301a1f209
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-11-29 09:12:53 +0100

    sidenav upon routing turning off the sidenav when routing ends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Event, Router, NavigationEnd } from '@angular/router';
 
 import { UserInterfaceService } from './services/user-interface.service';
 import { ProductsService } from './services/products.service';
@@ -12,9 +13,16 @@ export class AppComponent implements OnInit {
   userInterfaceState$ = this._userInterfaceService.userInterfaceState$;
 
   constructor(
+    private _router: Router,
     private _userInterfaceService: UserInterfaceService,
     private _productsService: ProductsService
-  ) {}
+  ) {
+    this._router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this._userInterfaceService.closeSidenav();
+      }
+    });
+  }
 
   handleClosingSidenav(): void {
     this._userInterfaceService.closeSidenav();
