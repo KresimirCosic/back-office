041fa357b485c8974bad7c08997b4685ba4ed026
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-11-30 14:17:48 +0100

    inital setup added products service to the dependecy

--- src/app/modules/products/components/pages/home/home.component.ts
+++ src/app/modules/products/components/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { IProductsState } from '../../../../../models/state/products-state.model';
+import { ProductsService } from '../../../../../services/products.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  productsState$: Observable<IProductsState>;
+
+  constructor(private _productsService: ProductsService) {
+    this.productsState$ = this._productsService.productsState$;
+  }
 
   ngOnInit(): void {}
 }
