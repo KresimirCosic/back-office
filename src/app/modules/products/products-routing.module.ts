f167743597c0139fb53d58c78bb064303f6e3d57
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-11-30 09:48:39 +0100

    stats page initialized code for the page, fixed the router link(s) in sidenav

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -4,9 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from '../../guards/authentication.guard';
 
 import { HomeComponent } from './components/pages/home/home.component';
+import { StatsComponent } from './components/pages/stats/stats.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthenticationGuard] },
+  {
+    path: 'stats',
+    component: StatsComponent,
+    canActivate: [AuthenticationGuard],
+  },
 ];
 
 @NgModule({
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { HomeComponent } from './components/pages/home/home.component';
+import { StatsComponent } from './components/pages/stats/stats.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, StatsComponent],
   imports: [SharedModule, ProductsRoutingModule],
 })
 export class ProductsModule {}
