d8208d087bbde7d104614d33095eb19ea778c424
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-12-01 16:33:52 +0100

    styling list of products overhaul and css fixes; some to other non-related components

--- src/app/modules/products/components/pages/products/products.component.ts
+++ src/app/modules/products/components/pages/products/products.component.ts
@@ -5,11 +5,11 @@ import { IProductsState } from '../../../../../models/state/products-state.model
 import { ProductsService } from '../../../../../services/products.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class ProductsComponent implements OnInit {
   productsState$: Observable<IProductsState>;
 
   constructor(private _productsService: ProductsService) {
