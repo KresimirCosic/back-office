f8241e0957b3e05f7e3c724cbc20d2620980b281
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-12-07 15:33:44 +0100

    pagination bug fix next, end buttons weren't disabled when they should've been

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDividerModule } from '@angular/material/divider';
 
 const materialUIModules = [
   MatInputModule,
   MatButtonModule,
   MatButtonToggleModule,
+  MatDividerModule,
 ];
 
 @NgModule({
